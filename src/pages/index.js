0e5bc0f2cece98cda01b76d80537c70c347948e1
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-12 10:17:50 +1200

    Refactored footer into a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import {Parallax, Background} from "react-parallax";
 import {Header, Menu, Container, Image, Segment, Grid, List, Icon} from "semantic-ui-react";
 import Link from "gatsby-link";
 
+import Footer from "../components/footer";
+
 import "../semantic/semantic.css"
 import "../styles/default.css";
 import "../styles/index.css";
@@ -50,40 +52,7 @@ export default () => (
 			</Grid>
 		</div>
 
-	{/** Footer */}
-		<Segment inverted vertical style={{ padding: '5em 0em' }}>
-		    <Container>
-		        <Grid divided inverted stackable>
-		            <Grid.Row>
-		                <Grid.Column width={3}>
-		                    <Header inverted as='h4' content='About' />
-		                    <List link inverted>
-		                        <List.Item as='a'>Sitemap</List.Item>
-		                        <List.Item as='a'>Contact Us</List.Item>
-		                        <List.Item as='a'>Religious Ceremonies</List.Item>
-		                        <List.Item as='a'>Gazebo Plans</List.Item>
-		                    </List>
-		                </Grid.Column>
-		                <Grid.Column width={3}>
-		                    <Header inverted as='h4' content='Services' />
-		                    <List link inverted>
-		                        <List.Item as='a'>Banana Pre-Order</List.Item>
-		                        <List.Item as='a'>DNA FAQ</List.Item>
-		                        <List.Item as='a'>How To Access</List.Item>
-		                        <List.Item as='a'>Favorite X-Men</List.Item>
-		                    </List>
-		                </Grid.Column>
-		                <Grid.Column width={7}>
-		                    <Header as='h4' inverted>
-		                        Footer Header
-		                    </Header>
-		                    <p>
-		                        Extra space for a call to action inside the footer that could help re-engage users.
-		                    </p>
-		                </Grid.Column>
-		            </Grid.Row>
-		        </Grid>
-		    </Container>
-		</Segment>
+		<Footer/>
+	
 	</div>
 );
