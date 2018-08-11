ac4c07394db0081fa994d1730cf9eb1a70918ad4
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-11 16:33:36 +1200

    Implemented basic parallax and images into the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from "react"
 import {Parallax, Background} from "react-parallax";
+import {Header} from "semantic-ui-react";
+
+import "../styles/default.css";
+import "../styles/index.css";
 
 export default () => (
-	<div>
-		<div>
-			<h1>Hello, I'm Jordan Sim-Smith</h1>
-		</div>
-		<div>
-			<h2>About me</h2>
-			<div>
-				<img src="https://source.unsplash.com/random/400x200" alt="" />
+	<Parallax
+		blur={0}
+		bgImage={require("../images/background.jpg")}
+		bgImageAlt=""
+		strength={200}
+	>
+			<div id="landing">
+				<Header as="h1">Hello, I'm Jordan Sim-Smith</Header>
 			</div>
 			<div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h2>About me</h2>
+				<div>
+					<img src={require("../images/portrait.jpg")} alt="" />
+				</div>
+				<div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
 			</div>
-		</div>
-	</div>
+	</Parallax>
 );
