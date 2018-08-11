4f2a958c60b890ea047f2c8e6c14b019d524e53f
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-11 15:53:39 +1200

    Created skeleton for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import {Parallax, Background} from "react-parallax";
 
-export default () => <div>Hello world!</div>
+export default () => (
+	<div>
+		<div>
+			<h1>Hello, I'm Jordan Sim-Smith</h1>
+		</div>
+		<div>
+			<h2>About me</h2>
+			<div>
+				<img src="https://source.unsplash.com/random/400x200" alt="" />
+			</div>
+			<div>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</div>
+);
