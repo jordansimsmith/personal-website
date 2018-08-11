76a134fbb72bc647f23adaf25d192da7fdb9b14d
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-12 10:10:41 +1200

    Body items refactored into a SUIR Grid. Added Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Parallax, Background} from "react-parallax";
-import {Header, Menu, Container, Image, Segment, Grid, List} from "semantic-ui-react";
+import {Header, Menu, Container, Image, Segment, Grid, List, Icon} from "semantic-ui-react";
 import Link from "gatsby-link";
 
 import "../semantic/semantic.css"
@@ -32,16 +32,25 @@ export default () => (
 		</Menu>
 
 		<div className="bg-light">
-			<h2>About me</h2>
-			<div className="image">
-				<img src={require("../images/portrait.jpg")} alt="" />
-			</div>
-			<div className="text">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-			</div>
+			<Header as="h2" icon textAlign="center">
+				<Icon name="user"/>
+				<Header.Content>About Me</Header.Content>
+			</Header>
+			<Grid divided inverted>
+				<Grid.Row>
+					<Grid.Column width={6}>
+						<Image src={require("../images/portrait.jpg")} />
+					</Grid.Column>
+					<Grid.Column width={10}>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+					</Grid.Column>
+
+				</Grid.Row>
+			</Grid>
 		</div>
 
+	{/** Footer */}
 		<Segment inverted vertical style={{ padding: '5em 0em' }}>
 		    <Container>
 		        <Grid divided inverted stackable>
