620f12b70b090f5cc8a1f53366d9143067695a82
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-29 21:01:17 +1200

    Wrote content for about me page. Refactored about me page to use React Item components instead of a grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Header, Menu, Container, Image, Segment, Grid, List, Icon} from "semantic-ui-react";
+import {Header, Menu, Container, Image, Segment, Grid, List, Icon, Item} from "semantic-ui-react";
 import Link from "gatsby-link";
 
 import Footer from "../components/footer";
@@ -11,7 +11,6 @@ import "../styles/index.css";
 
 export default () => (
 	<div>
-
 		<div className="bg">
 			<Header as="h1">Hello, I'm Jordan Sim-Smith</Header>
 		</div>
@@ -23,18 +22,21 @@ export default () => (
 				<Icon name="user"/>
 				<Header.Content>About Me</Header.Content>
 			</Header>
-			<Grid divided inverted>
-				<Grid.Row>
-					<Grid.Column width={6}>
-						<Image src={require("../images/portrait.jpg")} />
-					</Grid.Column>
-					<Grid.Column width={10}>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-					</Grid.Column>
-
-				</Grid.Row>
-			</Grid>
+			<Item.Group>
+				<Item>
+					<Item.Image size="medium" src={require("../images/portrait.jpg")} />
+					<Item.Content>
+						<Item.Header as="h3">Hi there!</Item.Header>
+						<Item.Description>
+						<p>My name is Jordan Sim-Smith, and I am a second-year Software Engineering student at the University of Auckland with a passion for learning. I enjoy working my way through challenging problems while teaching myself new skills at the same time. </p>
+						<p>I bring academic and industry experience to any task. My current position as a Software Engineer Intern at Autogrow Systems Limited allows me to apply skills and techniques that I have learned through university to real-world problems. </p>
+						<p>I am not solely interested in development, the financial aspect of a company intrigues me too. Alongside my study, I have cofounded and co-run a mobile phone trading/repairing company. I have a natural interest towards entrepreneurship, and I would love to create a startup company using the latest software.</p>
+						<p>One way that I like to challenge myself and learn new skills is to build personal projects. This website is an example of one; I am learning React.js as I go! I will add future projects to this website as they are ready. </p>
+						<p>Thank you for taking the time to learn a bit about me. Please do not hesitate to visit the contact section if you wish to send me an email. Have a good day. </p>
+						</Item.Description>
+					</Item.Content>
+				</Item>
+			</Item.Group>
 		</Container>
 
 		<Footer/>
