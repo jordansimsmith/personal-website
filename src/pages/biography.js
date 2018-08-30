459a4821e502c94d140380aac26fd176f6ad7fff
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-30 14:28:03 +1200

    Added content to the experience section on the biography page

--- src/pages/biography.js
+++ src/pages/biography.js
@@ -143,18 +143,30 @@ export default () => (
 
 			</Header>
 
-			<Grid divided inverted>
-				<Grid.Row>
-					<Grid.Column width={5}>
-						<Image src={require("../images/autogrow.jpg")} />
-					</Grid.Column>
-					<Grid.Column width={10}>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-						<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-					</Grid.Column>
+			<Item.Group>
+				<Item>
+					<Item.Image size="medium" src={require("../images/autogrow.jpg")} />
+					<Item.Content>
+						<Item.Header as="h4">Autogrow Systems Limited</Item.Header>
+						<Item.Meta>Software Engineer Intern, 2018 - PRESENT</Item.Meta>
+						<Item.Description>
+							<p>"Autogrow creates automation solutions that leverage the power of technology, data science and plant biology to provide growers affordable, accessible and easy-to-use innovation – 24/7, anywhere in the world." - autogrow.com</p>
+							<p>I am currently working as a Software Engineer Intern at Autogrow during my breaks from University. Through this position, I hope to learn new skills and gain experience in a professional software development environment. </p>
+							<p>So far, I have learnt many new skills, both technical and professional. This intern position has proved to be a great learning environment; I would learn something new every day at the office. 
+							It has been an excellent opportunity to apply skills developed through university to real products. It has been a great experience working with others in an agile setting, something that cannot be easily taught in a course.</p>
+							<p>At Autogrow, I have developed image recognition software using Node.js libraries. Our team followed a serverless approach facilitated by Amazon Web Services (AWS) Lamba and Step Functions.</p>
 
-				</Grid.Row>
-			</Grid>
+							<Header as="h4">Technologies Used</Header>
+							<List bulleted>
+								<List.Item>Node.js</List.Item>
+								<List.Item>Jira/Confluence</List.Item>
+								<List.Item>Git</List.Item>
+								<List.Item>AWS Lambda, Step Functions, S3</List.Item>
+							</List>
+						</Item.Description>
+					</Item.Content>
+				</Item>
+			</Item.Group>
 
 		</Container>
 
