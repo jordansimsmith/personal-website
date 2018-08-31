afe694a02deda3582ff03fe025597468fac80888
Jordan Sim-Smith <jordansimsmith@gmail.com>
2018-08-31 18:50:51 +1200

    Added linkedIn to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Container, Header, Icon, Form, Message} from "semantic-ui-react";
+import {Container, Header, Icon, Form, Message, Button} from "semantic-ui-react";
 
 import Footer from "../components/footer";
 import Navigation from "../components/navigation";
@@ -15,6 +15,14 @@ export default () => (
 				<Icon name="mail"/>
 				<Header.Content>Contact Me</Header.Content>
 			</Header>
+
+			<Header as="h3" textAlign="center">
+				<Header.Content>Message me on LinkedIn or send me an email below and I will get back to you.</Header.Content>
+			</Header>
+
+			<div className="center">
+				<Button as="a" href="https://www.linkedin.com/in/jordansimsmith/" target="_blank" color="blue">My LinkedIn</Button>
+			</div>
 
 			<Form method="POST" action="https://formspree.io/jordansimsmith@gmail.com">
 				<Form.Input type="text" name="name" placeholder="Your name" label="Name" required />
