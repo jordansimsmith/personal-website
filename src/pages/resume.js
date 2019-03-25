c69f2bd592f87343166bcf9708126596e77bfebb
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-03-25 16:52:59 +1300

    Updated resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,32 +1,41 @@
-import React from "react";
+import React from 'react';
 
-import {Container, Header, Icon, Image, Button, Segment} from "semantic-ui-react";
+import {Container, Header, Icon, Image, Button} from 'semantic-ui-react';
 
-import Footer from "../components/footer";
-import Navigation from "../components/navigation";
+import Footer from '../components/footer';
+import Navigation from '../components/navigation';
 
 export default () => (
-	<div>
-		<Navigation/>
-		
-		<Container className="content box">
-			<Header as="h2" icon textAlign="center">
-				<Icon name="file alternate"/>
-				<Header.Content>Resume</Header.Content>
-			</Header>
+  <div>
+    <Navigation />
 
-			<div className="center">
-			<Button as="a" href={require("../assets/jordan-sim-smith-cv.pdf")} download>
-				<Icon name="download"/>
-				Download PDF
-			</Button>
-			</div>
+    <Container className="content box">
+      <Header as="h2" icon textAlign="center">
+        <Icon name="file alternate" />
+        <Header.Content>Resume</Header.Content>
+      </Header>
 
-			<div className="center">
-				<Image id="resume" src={require("../images/cv.png")} fluid rounded bordered/>
-			</div>
-		</Container>
+      <div className="center">
+        <Button
+          as="a"
+          href={require('../assets/jordan-sim-smith-cv.pdf')}
+          download>
+          <Icon name="download" />
+          Download PDF
+        </Button>
+      </div>
 
-		<Footer/>
-	</div>
-	);
+      <div className="center">
+        <Image
+          id="resume"
+          src={require('../images/cv.png')}
+          fluid
+          rounded
+          bordered
+        />
+      </div>
+    </Container>
+
+    <Footer />
+  </div>
+);
