c8b72bb276be63a3540692393bd7c10145f11639
Jordan Sim-Smith <jordansimsmith@gmail.com>
2022-03-12 14:56:13 +1300

    Add some basic content

--- v2/pages/_app.tsx
+++ v2/pages/_app.tsx
@@ -1,8 +1,20 @@
+import { ChakraProvider, Container } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
+import { Navigation } from '../components/Navigation';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <header>
+        <Navigation />
+      </header>
+
+      <Container as="main" maxWidth="container.xl" marginTop="100px">
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
--- v2/pages/index.tsx
+++ v2/pages/index.tsx
@@ -1,5 +1,25 @@
+import { Box, Heading, Link, Text } from '@chakra-ui/react';
+
 const Home = (): JSX.Element => {
-  return <div>hello world</div>;
+  return (
+    <Box>
+      <Heading size="3xl">
+        A developer passionate about creating modern web services
+      </Heading>
+
+      <Text marginTop="20px">
+        I'm an Engineer at{' '}
+        <Link href="https://www.nintex.com/" isExternal color="green.600">
+          Nintex
+        </Link>{' '}
+        and a graudate from the{' '}
+        <Link href="https://www.auckland.ac.nz/" isExternal color="green.600">
+          University of Auckland
+        </Link>
+        .
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
