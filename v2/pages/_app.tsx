f0ca6e037fdfcd4ac2d93f07819dc25ada7863f7
Jordan Sim-Smith <jordansimsmith@gmail.com>
2022-03-17 14:39:34 +1300

    Add favicon

--- v2/pages/_app.tsx
+++ v2/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NextHead from 'next/head';
 import { ChakraProvider, Container, Flex } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { Footer } from '../components/Footer';
@@ -7,6 +8,27 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ChakraProvider>
+      <NextHead>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </NextHead>
+
       <Flex direction="column">
         <Header />
 
